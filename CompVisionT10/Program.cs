6a20751a7da54a173ee32749c69dfc9da6f9d162
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-30 17:29:26 +0300

    Initial commit

---
+++ CompVisionT10/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace ComputerVision
+{
+    class Program
+    {
+        static string subscriptionKey = "1464b114a4cf445598797e54f567c975";
+        static string endpoint = "https://t10dovbush.cognitiveservices.azure.com/";
+        // URL image used for analyzing an image (image of puppy)
+        private const string ANALYZE_URL_IMAGE = "http://chudo.tech/wp-content/uploads/2019/02/MuskTesla.jpg";
+
+        static void Main(string[] args)
+        {
+            // Create a client
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+            // Analyze an image to get features and other properties.
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
+            Console.WriteLine();
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+
+            // Sunmarizes the image content.
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+
+            // Display categories the image is divided into.
+            Console.WriteLine("Categories:");
+            foreach (var category in results.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+
+            // Image tags and their confidence score
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Objects
+            Console.WriteLine("Objects:");
+            foreach (var obj in results.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
+                  $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Well-known (or custom, if set) brands.
+            Console.WriteLine("Brands:");
+            foreach (var brand in results.Brands)
+            {
+                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                  $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Faces
+            Console.WriteLine("Faces:");
+            foreach (var face in results.Faces)
+            {
+                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
+                  $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
+                  $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+            }
+            Console.WriteLine();
+
+            // Adult or racy content, if any.
+            Console.WriteLine("Adult:");
+            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence {results.Adult.AdultScore}");
+            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
+            Console.WriteLine($"Has gory content: {results.Adult.IsGoryContent} with confidence {results.Adult.GoreScore}");
+            Console.WriteLine();
+
+            // Identifies the color scheme.
+            Console.WriteLine("Color Scheme:");
+            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
+            Console.WriteLine("Accent color: " + results.Color.AccentColor);
+            Console.WriteLine("Dominant background color: " + results.Color.DominantColorBackground);
+            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
+            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
+            Console.WriteLine();
+        }
+    }
+}
